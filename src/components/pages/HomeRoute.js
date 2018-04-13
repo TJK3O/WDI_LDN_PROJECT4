7933776cd5855b5ed3247d749be6ddfe0a8d2f63
Tom Keogh <thomas.keogh@me.com>
2018-04-13 09:27:57 +0100

    Changed homepage text

--- src/components/pages/HomeRoute.js
+++ src/components/pages/HomeRoute.js
@@ -7,13 +7,10 @@ class HomeRoute extends React.Component {
       <section className="homepage-container">
         <h1 className="home-main-logo">c o n t e n t</h1>
 
-        <p>Do you worry about missing out on the latest music, films, and tv shows? Use content to keep track of all the content you want to see and hear.</p>
+        <p>Use content to keep track of all the content you want to see and hear.</p>
 
-        <p>Add items to your to do list, tick them off and get rewarded with badge.</p>
+        <p>Add, share, consume</p>
 
-        <p>Follow other users to see what content people are enjoying.</p>
-
-        <p>Suggest content to that other users might enjoy.</p>
         <p>be content :)</p>
       </section>
     );
