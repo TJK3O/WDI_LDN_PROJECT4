7656ee84fc85de3646790a40aa3f96db6dff2fb4
Tom Keogh <thomas.keogh@me.com>
2018-04-26 11:31:41 +0100

    Commented out Flash and FlashMessages

--- src/components/common/FlashMessages.js
+++ src/components/common/FlashMessages.js
@@ -1,16 +1,21 @@
 import React from 'react';
+// Flash gives us some static functions for setting, getting and clearing messages
 import Flash from '../../lib/Flash';
 
+
+
+//---------------------------------------//
 class FlashMessages extends React.Component {
 
   render() {
-
+    // We store the current messages in a variable before clearing the messages
     const messages = Flash.getMessages();
     Flash.clearMessages();
 
     return (
       <div className="container">
         {messages && Object.keys(messages).map((type, i) =>
+          // We display the value associated with the key type
           <div key={i} className="flash">{messages[type]}</div>
         )}
       </div>
--- src/lib/Flash.js
+++ src/lib/Flash.js
@@ -2,7 +2,10 @@ class Flash {
 
   _messages = null;
 
+  // Static method calls are made directly on the class and are not callable on instances of the class. Static methods are often used to create utility functions. Static methods are not directly accessible using the this keyword from non-static methods. You need to call them using the class name: CLASSNAME.STATIC_METHOD_NAME() or by calling the method as a property of the constructor: this.constructor.STATIC_METHOD_NAME(). This does the same as assigning it as a function property e.g. User.staticMethod = function()
+
   static setMessage(type, message) {
+    // if there aren't any messages then this._messages becomes an empty object
     this._messages = this._messages || {};
     this._messages[type] = message;
   }
